5fc357bea7db543e6bfc6dbbbf06d1593131e849
Andrew Matthews <matthews.andrew@gmail.com>
2018-03-22 09:55:00 +1100

    wip

--- init/rc1.d/01-functions.fish
+++ init/rc1.d/01-functions.fish
@@ -1,19 +1,63 @@
 #!/usr/bin/fish
 
-# assoc usage patterns
-# ==========================
-# assoc.set name[key] value
-# assoc.get name[key]
-# assoc.rm  name[key]
-# assoc.has_key name[key]
-
-function project_path
-    set -l key $argv[1]
-    echo (assoc.get project_paths[$key])
+function project
+  if test 0 -eq (count $argv)
+    project_help
+    return
+  end
+  switch $argv[1]
+    case home
+        project_help
+    case help
+        project_help
+    case '*'
+      project_help
+  end
 end
 
-function project_name
-    echo (assoc.get project_names[$argv[1]])
+function project_help -d "display usage info"
+  echo "Fishdots project Usage"
+  echo "======================"
+  echo "project <command> [options] [args]"
+  echo ""
+  echo "project edit pattern"
+  echo "  edit the project identified by the path"
+  echo ""
+  echo "project find pattern"
+  echo "  find the project by searching file names"
+  echo ""
+  echo "project search pattern"
+  echo "  perform a full text search for patterns"
+  echo ""
+  echo "project create title"
+  echo "  create a new project"
+  echo ""
+  echo "project pcreate title"
+  echo "  create a new project within a project area"
+  echo ""
+  echo "project save"
+  echo "  save any new or modified projects locally"
+  echo ""
+  echo "project move"
+  echo "  explain,,,"
+  echo ""
+  echo "project sync"
+  echo "  synchronise projects with origin git repo"
+  echo ""
+  echo "project home"
+  echo "  cd to the projects directory"
+  echo ""
+  echo "project help"
+  echo "  EXPL"
+end
+
+
+function project_path -a project_name
+    echo (assoc.get project_paths[$project_name])
+end
+
+function project_name -a project_name
+    echo (assoc.get project_names[$project_name])
 end
 
 function project_list_project_short_names
@@ -28,14 +72,14 @@ function project_list_project_long_names
     end
 end
 
-function goto
-    ok "Switching to "(project_name $argv[1])
-    set_current_project $argv[1]
-    pcd $argv[1]
+function goto -a project_name
+    ok "Switching to "(project_name $project_name)
+    set_current_project $project_name
+    pcd $project_name
 end
 
-function set_current_project
-    set -x CURRENT_PROJECT_SN $argv[1]
+function set_current_project -a project_name
+    set -U CURRENT_PROJECT_SN $project_name
 end
 
 function edit_project
@@ -47,23 +91,23 @@ function _cd_current_project_directory
     _cd_project_directory $CURRENT_PROJECT_SN
 end
 
-function _cd_project_directory
-    cd (project_path $argv[1])
+function _cd_project_directory -a project_name
+    cd (project_path $project_name)
 end
 
 
-function _create_project_task
-    _create_task "$argv[1] +$CURRENT_PROJECT_SN"
+function _create_project_task -a title
+    _create_task "$title +$CURRENT_PROJECT_SN"
 end
 
-function _create_project_note_dated
-    notes n "$CURRENT_PROJECT_SN/"(date --iso-8601)"-$argv[1]"
+function _create_project_note_dated -a project_name
+    notes n "$CURRENT_PROJECT_SN/"(date --iso-8601)"-$project_name"
 end
 
-function _project_quick_checkin
+function _project_quick_checkin -a checkin_message
     git add -A
     git rm (git ls-files --deleted) 2> /dev/null
-    git commit --no-verify -m "$argv[1]"
+    git commit --no-verify -m "$checkin_message"
     git push origin (git branch-name)
 end
 
@@ -75,7 +119,7 @@ function _list_project_tasks
     td list +$CURRENT_PROJECT_SN
 end
 
-function pcd -d "switch to project directory"
-    cd (project_path $argv[1])
+function pcd -a project_name -d "switch to project directory"
+    cd (project_path $project_name)
 end
 
