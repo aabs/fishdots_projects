0cb70d6510a110a9182281bc7aaa3659764ad177
Andrew Matthews <matthews.andrew@gmail.com>
2018-03-26 09:38:11 +1100

    wip

--- init/rc0.d/01-env.fish
+++ init/rc0.d/01-env.fish
@@ -1,2 +1,7 @@
-set -x PROJECT_HOME $SYNC_HOME/active_personal/Projects
-set -x CURRENT_PROJECT_SN dots
+if not set -q $PROJECT_HOME
+    set -U PROJECT_HOME $SYNC_HOME/active_personal/Projects
+end
+
+if not set -q $CURRENT_PROJECT_SN
+    set -U CURRENT_PROJECT_SN dots
+end
--- init/rc1.d/02-abbr.fish
+++ init/rc1.d/02-abbr.fish
@@ -3,13 +3,10 @@ abbr --add ptask        '_create_project_task' # create a task for a specific pr
 abbr --add ptasks       "_list_project_tasks"
 
 abbr --add projects     'project_list_project_short_names'
-abbr --add mltp     'cd $CAREER_DIR && atom .'
+abbr --add mltp         'goto notes'
 abbr --add pnote        '_create_project_note_dated'
 function _find_project_notes
     notes find $CURRENT_PROJECT_SN
-end
-function project 
-    echo $CURRENT_PROJECT_SN
 end
 abbr --add pcd      '_cd_project_directory'
 abbr --add pqci     '_project_quick_checkin'
