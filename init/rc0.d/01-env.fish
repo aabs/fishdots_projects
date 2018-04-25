78483f478af0efafd25afd09c91437bfefc37095
Andrew Matthews <matthews.andrew@gmail.com>
2018-04-26 08:49:02 +1000

    corrected merge issue

--- init/rc0.d/01-env.fish
+++ init/rc0.d/01-env.fish
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 if not set -q $PROJECT_HOME
     set -U PROJECT_HOME $SYNC_HOME/active_personal/Projects
 end
@@ -6,7 +5,4 @@ end
 if not set -q $CURRENT_PROJECT_SN
     set -U CURRENT_PROJECT_SN dots
 end
-=======
-set -x PROJECT_HOME $SYNC_HOME/active_personal/Projects
-set -x CURRENT_PROJECT_SN fd
->>>>>>> 0addbf7090e9c7485d9e9dd9b3febf677dbe0f5a
+
