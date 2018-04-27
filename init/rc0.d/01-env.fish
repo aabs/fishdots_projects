00e3309cb7ef47b91d4baf03b15736b08c007aae
Andrew Matthews <matthews.andrew@gmail.com>
2018-04-27 10:05:54 +1000

    overriding ambient env setting for PROJECT_HOME

--- init/rc0.d/01-env.fish
+++ init/rc0.d/01-env.fish
@@ -1,6 +1,4 @@
-if not set -q $PROJECT_HOME
-    set -U PROJECT_HOME $SYNC_HOME/active_personal/Projects
-end
+set -U PROJECT_HOME $SYNC_HOME/active_personal/Projects
 
 if not set -q $CURRENT_PROJECT_SN
     set -U CURRENT_PROJECT_SN dots
