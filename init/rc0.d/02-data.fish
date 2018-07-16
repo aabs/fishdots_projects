7d288cc3ec6d388815f1f3e4d9e09c7ac77993d3
Andrew Matthews <andrew.matthews@team.telstra.com.au>
2018-07-16 14:07:26 +1000

    wip

--- init/rc0.d/02-data.fish
+++ init/rc0.d/02-data.fish
@@ -6,34 +6,19 @@
 # assoc.rm  name[key]
 # assoc.has_key name[key]
 
-    set _project_names coins erl fd career thor notes kops probs fdp fdproj tlive ngoss relay
-
+    set _project_names fd thor probs fdp fdproj tlive ngoss relay
+# old porject names:  coins career notes kops  erl 
     assoc.set project_paths[tlive]   "$HOME/dev/by-technology/elixir/tlive"    
     assoc.set project_names[tlive]  "TLive 2 Development"    
-    
-    assoc.set project_paths[coins]   "$FINANCE_DIR"    
-    assoc.set project_names[coins]  "Ledger crypto-coin accounts"    
-    
-    assoc.set project_paths[erl]     "$PROJECT_HOME/by-technology/erlang/katas"   
-    assoc.set project_names[erl]    "Erlang Code Katas"	
 
     assoc.set project_paths[fd]    "$FISHDOTS" 
     assoc.set project_names[fd]   "My Fish Dotfiles"    
-    
-    assoc.set project_paths[career]  "$CAREER_DIR" 
-    assoc.set project_names[career] "My Career Plan" 
-
-    assoc.set project_paths[notes]   "$HOME/notes" 
-    assoc.set project_names[notes]  "My Text Notes Collection" 
 
     assoc.set project_paths[probs]   "$FD_PROBLEM_HOME" 
     assoc.set project_names[probs]  "The Problems root directory" 
 
-    assoc.set project_paths[fdp]    "$HOME/dev/fishdots_problem"
+    assoc.set project_paths[fdp]    "$FISHDOTS_PLUGINS_HOME/fd_problem"
     assoc.set project_names[fdp]   "Fishdots Problem Plugin"
-
-    assoc.set project_paths[kops]    "/mnt/d/Synchronised/active_personal/Projects/by-technology/docker/kubernetes/kops"
-    assoc.set project_names[kops]   "Kubernetes kops experiments"
 
     assoc.set project_paths[fdproj]    "$FISHDOTS_PLUGINS_HOME/fishdots_projects"
     assoc.set project_names[fdproj]   "Fishdots plugin for projects"
@@ -46,3 +31,23 @@
 
     assoc.set project_paths[thor]    "$PROJECT_HOME/by-technology/elixir/thor/" 
     assoc.set project_names[thor]   "THOR home"	
+
+
+################
+# OLD STUFF
+################
+
+# assoc.set project_paths[kops]    "/mnt/d/Synchronised/active_personal/Projects/by-technology/docker/kubernetes/kops"
+# assoc.set project_names[kops]   "Kubernetes kops experiments"
+
+# assoc.set project_paths[notes]   "$HOME/notes" 
+# assoc.set project_names[notes]  "My Text Notes Collection" 
+
+# assoc.set project_paths[career]  "$CAREER_DIR" 
+# assoc.set project_names[career] "My Career Plan" 
+
+# assoc.set project_paths[erl]     "$PROJECT_HOME/by-technology/erlang/katas"   
+# assoc.set project_names[erl]    "Erlang Code Katas"	
+
+# assoc.set project_paths[coins]   "$FINANCE_DIR"    
+# assoc.set project_names[coins]  "Ledger crypto-coin accounts"    
