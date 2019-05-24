723414b2623dd944cf63693318022a1382f9deeb
Andrew Matthews <andrew.matthews@team.telstra.com.au>
2019-05-24 13:52:18 +1000

    more tinkering

--- init/rc1.d/03-prompt.fish
+++ init/rc1.d/03-prompt.fish
@@ -12,6 +12,11 @@ function fish_mode_prompt
     set_color -b green
     echo -n $K8S_NS" "
   end 
+  if set -q -U K8S_NS
+    set_color black
+    set_color -b white
+    echo -n "~> "(get_ssh_host)" "
+  end 
   set_color normal
   set_color -b normal
 end
